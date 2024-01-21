8bf78155329a2c8411e84fef584cf8a7cae14b01
isixe <isixe@outlook.com>
2024-01-21 11:54:09 +0800

    :zap: perf(entity): update response code

--- springboot/src/main/java/dev/itea/echo/entity/result/ResultCode.java
+++ springboot/src/main/java/dev/itea/echo/entity/result/ResultCode.java
@@ -23,7 +23,7 @@ public enum ResultCode {
 
     /*用户错误：2001-2999*/
     USER_NOT_LOGGED_IN(2001, "用户未登录，请登录后重试"),
-    USER_LOGIN_ERROR(2002, "账号不存在或密码错误"),
+    USER_LOGIN_ERROR(2002, "用户名或密码错误"),
     USER_ACCOUNT_FORBIDDERN(2003, "账号已被禁用"),
     USER_NOT_EXIST(2004, "用户不存在"),
     USER_HAS_EXISTED(2005, "账号已存在"),
