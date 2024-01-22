108a57b39cfd3c1729525ab1a57bd98fb7a9f3da
isixe <isixe@outlook.com>
2024-01-22 18:31:30 +0800

    :memo: doc(vaildation): update head doc for group vaildation

--- springboot/src/main/java/dev/itea/echo/validation/AddValidationGroup.java
+++ springboot/src/main/java/dev/itea/echo/validation/AddValidationGroup.java
@@ -1,9 +1,10 @@
 package dev.itea.echo.validation;
 
 /**
+ * 数据增加的分组校验
+ *
  * @author: isixe
  * @create: 2023-06-25 01:08
- * @description: 数据增加的分组校验
  **/
 public interface AddValidationGroup {
 }
--- springboot/src/main/java/dev/itea/echo/validation/UpdateValidationGroup.java
+++ springboot/src/main/java/dev/itea/echo/validation/UpdateValidationGroup.java
@@ -1,9 +1,10 @@
 package dev.itea.echo.validation;
 
 /**
+ * 数据更新的分组校验
+ *
  * @author: isixe
  * @create: 2023-06-25 01:07
- * @description: 数据更新的分组校验
  **/
 public interface UpdateValidationGroup {
 }
