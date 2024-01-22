4482e3cb609ed0350bf77cd2582fdca62df4f7b5
isixe <isixe@outlook.com>
2024-01-22 17:59:32 +0800

    :white_check_mark: test(test): add redis connection test

--- springboot/src/test/java/dev/itea/echo/BaseApplicationTests.java
+++ springboot/src/test/java/dev/itea/echo/BaseApplicationTests.java
@@ -5,10 +5,12 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.Duration;
 
 /**
  * @author: isixe
@@ -20,25 +22,42 @@ import java.sql.SQLException;
 class BaseApplicationTests {
     @Resource
     private DataSource dataSource;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void contextLoads() {
-        System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
+        log.info("Sa-Token init: " + SaManager.getConfig());
     }
 
+    /**
+     * MySQL Test
+     */
     @Test
     void datasourceLoads() {
-        //查看数据源
-        log.info(String.valueOf(dataSource.getClass()));
-        //获得数据库连接
+        //check datasource
+        log.info("MySQL dataSource init: " + dataSource.getClass());
         try {
+            //get connection
             Connection connection = dataSource.getConnection();
-            log.info(String.valueOf(connection));
-            //关闭连接
+            log.info("MySQL connection: " + connection);
+            //close
             connection.close();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Redis Test
+     */
+    @Test
+    void redisLoads() {
+        //set redis
+        log.info("Redis Template init: " + stringRedisTemplate);
+        stringRedisTemplate.opsForValue().set("redis", "Redis init success", Duration.ofMinutes(1));
+        //get
+        String value = this.stringRedisTemplate.opsForValue().get("redis");
+        log.info("Init Result={}", value);
     }
 }
