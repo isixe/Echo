842ffcbbd056a904f4fe4acfdd028bc543fa7ee0
isixe <isixe@outlook.com>
2024-01-25 17:55:33 +0800

    🔧 build(package.json): add autoAnimatePlugin

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
 import App from './App.vue'
 import router from './router'
@@ -10,6 +11,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(autoAnimatePlugin)
 app.use(router)
 
 app.mount('#app')
