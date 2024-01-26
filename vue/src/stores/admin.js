58f7ef6b68afbbfe76fb6ba60153d6d0695b450a
isixe <isixe@outlook.com>
2024-01-26 11:12:02 +0800

    🎈 perf(store): enable store persisted

--- vue/src/stores/admin.js
+++ vue/src/stores/admin.js
@@ -1,35 +1,42 @@
 import { defineStore } from 'pinia'
 import { login, logout } from '@/api/admin'
 
-export const useAdminStore = defineStore('admin', () => {
-  //state
-  const id = ref()
-  const tokenName = ref()
-  const tokenValue = ref()
-  const tokenTimeout = ref()
+export const useAdminStore = defineStore(
+  'admin',
+  () => {
+    //state
+    const id = ref()
+    const tokenName = ref()
+    const tokenValue = ref()
+    const tokenTimeout = ref()
 
-  //action
-  const loginAction = async (data) => {
-    const formData = new URLSearchParams()
-    formData.append('name', data.name)
-    formData.append('password', data.password)
-    formData.append('rememberMe', data.rememberMe)
+    //action
+    const loginAction = async (data) => {
+      const formData = new URLSearchParams()
+      formData.append('name', data.name)
+      formData.append('password', data.password)
+      formData.append('rememberMe', data.rememberMe)
 
-    await login(formData).then((response) => {
-      id.value = response.data.loginId
-      tokenName.value = response.data.tokenName
-      tokenValue.value = response.data.tokenValue
-      tokenTimeout.value = response.data.tokenTimeout
-    })
+      await login(formData).then((response) => {
+        id.value = response.data.loginId
+        tokenName.value = response.data.tokenName
+        tokenValue.value = response.data.tokenValue
+        tokenTimeout.value = response.data.tokenTimeout
+      })
+    }
+
+    const logoutAction = async () => {
+      await logout().then(() => {
+        id.value = null
+        tokenName.value = null
+        tokenValue.value = null
+        tokenTimeout.value = null
+      })
+    }
+
+    return { id, tokenName, tokenValue, tokenTimeout, loginAction, logoutAction }
+  },
+  {
+    persist: true
   }
-
-  const logoutAction = async () => {
-    await logout().then(() => {
-      tokenName.value = null
-      tokenValue.value = null
-      tokenTimeout.value = null
-    })
-  }
-
-  return { id, tokenName, tokenValue, tokenTimeout, loginAction, logoutAction }
-})
+)
