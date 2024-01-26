563eb7de37501b6dbccd5176701c20658c7c3fc5
isixe <isixe@outlook.com>
2024-01-26 11:10:43 +0800

    ✨ feat(main.js): use pinia-plugin-persistedstate for pina

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,13 +4,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pina = createPinia().use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pina)
 app.use(autoAnimatePlugin)
 app.use(router)
 
