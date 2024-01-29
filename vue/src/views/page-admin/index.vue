f819822a6981912029cbaf9727f4a6572e2b4652
isixe <isixe@outlook.com>
2024-01-29 19:08:01 +0800

    ✨ feat(view): provide search function for tables

--- vue/src/views/page-admin/index.vue
+++ vue/src/views/page-admin/index.vue
@@ -2,14 +2,17 @@
 	<a-layout>
 		<SideBar :isCollapsed="isCollapsed"></SideBar>
 		<a-layout>
-			<Header :isCollapsed="isCollapsed" @iconClick="() => isCollapsed = !isCollapsed"></Header>
-			<Main></Main>
+			<Header :isCollapsed="isCollapsed" @iconClick="() => isCollapsed = !isCollapsed"
+				@onSearch="(text) => searchText = text">
+			</Header>
+			<Main :searchText="searchText"></Main>
 		</a-layout>
 	</a-layout>
 </template>
 
 <script setup>
-import { SideBar, Header, Main } from "./components"
+import { SideBar, Header, Main } from './components'
 
+const searchText = ref()
 const isCollapsed = ref(false)
 </script>
