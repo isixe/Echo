c7ccd879f573191a8fa94b8570a4d64f9027b430
isixe <isixe@outlook.com>
2024-02-03 18:20:13 +0800

    ✨ feat(api): add user request api

--- vue/src/api/user.js
+++ vue/src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export async function login(data) {
   return request({
-    url: '/login',
+    url: '/user/login',
     method: 'post',
     data
   })
@@ -10,14 +10,47 @@ export async function login(data) {
 
 export function logout() {
   return request({
-    url: '/logout',
+    url: '/user/logout',
     method: 'post'
   })
 }
 
-export function register() {
+export function add(data) {
   return request({
-    url: '/register',
-    method: 'post'
+    url: '/user',
+    method: 'post',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: '/user',
+    method: 'put',
+    data
+  })
+}
+
+export function remove(data) {
+  return request({
+    url: '/user',
+    method: 'delete',
+    data
+  })
+}
+
+export function get(params) {
+  return request({
+    url: '/user',
+    method: 'get',
+    params
+  })
+}
+
+export function getUserList(params) {
+  return request({
+    url: '/user/queryAll',
+    method: 'get',
+    params
   })
 }
