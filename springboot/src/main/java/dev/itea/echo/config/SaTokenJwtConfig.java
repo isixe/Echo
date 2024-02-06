3bcd819ea7911826fd8c37256ef46c704f643395
isixe <isixe@outlook.com>
2024-02-06 16:49:11 +0800

    :bug: fix(config): fixed the failure of JWT in the multi -account token

--- springboot/src/main/java/dev/itea/echo/config/SaTokenJwtConfig.java
+++ springboot/src/main/java/dev/itea/echo/config/SaTokenJwtConfig.java
@@ -2,6 +2,8 @@ package dev.itea.echo.config;
 
 import cn.dev33.satoken.jwt.StpLogicJwtForSimple;
 import cn.dev33.satoken.stp.StpLogic;
+import dev.itea.echo.entity.StpUserUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +15,20 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 public class SaTokenJwtConfig {
+
+    /**
+     * set jwt mode
+     */
     @Bean
     public StpLogic getStpLogicJwt() {
         return new StpLogicJwtForSimple();
+    }
+
+    /**
+     * multi account support
+     */
+    @Autowired
+    public void setUserStpLogic() {
+        StpUserUtil.setStpLogic(new StpLogicJwtForSimple(StpUserUtil.TYPE));
     }
 }
