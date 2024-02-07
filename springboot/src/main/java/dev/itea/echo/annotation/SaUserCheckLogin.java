ec495f7b9bff84211cb19f20398516c8f5fa7762
isixe <isixe@outlook.com>
2024-02-07 15:03:09 +0800

    :sparkles: feat(config&annotation): add SaUserCheckLogin annotation

---
+++ springboot/src/main/java/dev/itea/echo/annotation/SaUserCheckLogin.java
@@ -0,0 +1,21 @@
+package dev.itea.echo.annotation;
+
+import cn.dev33.satoken.annotation.SaCheckLogin;
+import dev.itea.echo.entity.StpUserUtil;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * SaUserCheckLogin
+ *
+ * @author: isixe
+ * @create: 2024-02-07 13:13
+ **/
+@SaCheckLogin(type = StpUserUtil.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.TYPE})
+public @interface SaUserCheckLogin {
+}
--- springboot/src/main/java/dev/itea/echo/config/SaTokenConfig.java
+++ springboot/src/main/java/dev/itea/echo/config/SaTokenConfig.java
@@ -1,7 +1,10 @@
 package dev.itea.echo.config;
 
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.strategy.SaStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +16,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  **/
 @Configuration
 public class SaTokenConfig implements WebMvcConfigurer {
+
+    @Autowired
+    public void rewriteSaStrategy() {
+        SaStrategy.instance.getAnnotation = AnnotatedElementUtils::getMergedAnnotation;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SaInterceptor()).addPathPatterns("/api/**");
