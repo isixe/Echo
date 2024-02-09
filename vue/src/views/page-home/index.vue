c41f58584ec4e5c1f9c57881786d873e6580f637
isixe <isixe@outlook.com>
2024-02-09 20:13:41 +0800

    ✨ feat(view): use base frame

--- vue/src/views/page-home/index.vue
+++ vue/src/views/page-home/index.vue
@@ -1,3 +1,11 @@
 <template>
-    test
+    <a-layout>
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+    </a-layout>
 </template>
+
+<script setup>
+import { Header, Main, Footer } from './components'
+</script>
