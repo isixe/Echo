bc8ee489ce684ac382e394cb709dfc06075b17d7
isixe <isixe@outlook.com>
2024-02-10 20:21:28 +0800

    🎈 perf(config): update eslint and prettier config

--- vue/.eslintrc.cjs
+++ vue/.eslintrc.cjs
@@ -1,23 +1,25 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-	root: true,
-	extends: [
-		"./.eslintrc-auto-import.json",
-		"plugin:vue/vue3-essential",
-		"eslint:recommended",
-		"@vue/eslint-config-prettier/skip-formatting",
-	],
-	parserOptions: {
-		ecmaVersion: "latest",
-	},
-	rules: {
-		"vue/multi-word-component-names": [
-			"error",
-			{
-				ignores: ["index", "Header", "Main", "Footer"],
-			},
-		],
-	},
-};
+  root: true,
+  extends: [
+    './.eslintrc-auto-import.json',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['index', 'Header', 'Main', 'Footer']
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
