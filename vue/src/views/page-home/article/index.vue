afabf10ec90f49e06196e0d33a17ee93411cf929
isixe <isixe@outlook.com>
2024-02-14 11:06:21 +0800

    ✨ feat(view): show the user ranking data to the view

---
+++ vue/src/views/page-home/article/index.vue
@@ -0,0 +1,321 @@
+<template>
+  <!-- <div class="nav-box">
+        <div class="category-tags-container">
+            <div class="categories">
+                <span class="tag-category">
+                    <ClockCircleOutlined /> 历史
+                </span>
+                <span class="tag-category">
+                    <PlusOutlined /> 关注
+                </span>
+                <span class="tag-category">
+                    <HeartOutlined /> 收藏
+                </span>
+                <span class="tag-category">
+                    <LikeOutlined /> 点赞
+                </span>
+            </div>
+        </div>
+    </div> -->
+  <div class="content-box">
+    <div class="article-container">
+      <a-layout>
+        <a-layout-content class="article-items-container">
+          <a-menu
+            class="nav-menu"
+            v-model:selectedKeys="current"
+            mode="horizontal"
+            :items="items"
+          />
+          <EntryItem v-for="n in 10"></EntryItem>
+        </a-layout-content>
+        <a-layout-sider class="sidebar-container" width="320px">
+          <div class="notice-card">
+            <div class="notice-title">
+              <span>公告🔉</span>
+            </div>
+            <div class="notice-content">
+              <RouterLink class="notice-item" to="/article/3246543">
+                <span class="article-title"> 功能维护中... </span>
+                <span class="article-time"> 2024-02-01 </span>
+              </RouterLink>
+            </div>
+          </div>
+          <div class="rank-card">
+            <div class="rank-title">
+              <span
+                >发文榜
+                <font-awesome-icon
+                  :icon="['fas', 'ranking-star']"
+                  style="color: #c9c9c9; font-size: 14px; margin-left: 0"
+                />
+              </span>
+            </div>
+            <div class="rank-content">
+              <RouterLink
+                class="rank-item"
+                to="/user/xxx"
+                v-for="rankItem in rankList"
+                :key="rankItem.id"
+              >
+                <div class="user-info">
+                  <a-avatar class="user-avatar" :size="35" :src="rankItem.avatar">
+                    <template #icon>
+                      <UserOutlined />
+                    </template>
+                  </a-avatar>
+                  <div class="user-content">
+                    <span class="user-name">{{ rankItem.name }}</span>
+                    <span class="user-desc">{{ rankItem.description }}</span>
+                  </div>
+                </div>
+                <div class="article-count">{{ rankItem.rankSum }}</div>
+              </RouterLink>
+            </div>
+          </div>
+          <div class="recommend-category-card">
+            <div class="recommend-title">
+              <span>
+                推荐分类
+                <font-awesome-icon
+                  :icon="['fas', 'tags']"
+                  style="color: #c9c9c9; font-size: 14px; margin-left: 0"
+                />
+              </span>
+            </div>
+            <div class="recommend-content">
+              <RouterLink to="/categoty/xxx"> 类别1 </RouterLink>
+              <RouterLink to="/categoty/xxx"> 类别1 </RouterLink>
+              <RouterLink to="/categoty/xxx"> 类别1 </RouterLink>
+              <RouterLink to="/categoty/xxx"> 类别1 </RouterLink>
+              <RouterLink to="/categoty/xxx"> 类别1 </RouterLink>
+              <RouterLink to="/categoty/xxx"> 类别 </RouterLink>
+            </div>
+          </div>
+        </a-layout-sider>
+      </a-layout>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { EntryItem } from '@/views/page-home/components'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { getUserRank } from '@/api/article'
+import { faTags, faBullhorn, faRankingStar } from '@fortawesome/free-solid-svg-icons'
+library.add(faTags, faBullhorn, faRankingStar)
+
+const current = ref(['latest'])
+
+const items = ref([
+  {
+    key: 'latest',
+    label: '最新'
+  },
+  {
+    key: 'recommend',
+    label: '推荐'
+  },
+  {
+    key: 'hot',
+    label: '热门'
+  },
+  {
+    key: 'subscribe',
+    label: '订阅'
+  }
+])
+
+const rankList = ref([])
+
+onMounted(() => {
+  getUserRank().then((res) => {
+    console.log(res.data)
+    rankList.value = res.data
+  })
+})
+</script>
+
+<style scoped>
+.nav-box {
+  background-color: #ffffff;
+}
+
+.content-box {
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+
+.category-tags-container {
+  align-items: center;
+  display: flex;
+  height: 48px;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 1200px;
+  background-color: #ffffff;
+}
+
+.nav-menu {
+  font-size: 16px;
+  padding: 4px 0;
+}
+
+.article-container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+
+.tag-category {
+  color: #666;
+  cursor: pointer;
+  font-size: 15px;
+  padding: 4px 14px;
+  margin-right: 10px;
+}
+
+.tag-category:hover {
+  background-color: #f1f1ff;
+  border-radius: 4px;
+  color: #4d45e5;
+}
+
+.active-category {
+  background-color: #4d45e5 !important;
+  border-radius: 4px;
+  color: #ffffff !important;
+  font-weight: 700;
+}
+
+.article-items-container {
+  border-radius: 4px;
+  min-height: 80vh;
+  margin-right: 15px;
+  border-radius: 4px;
+  background-color: #ffffff;
+}
+
+.rank-item:hover {
+  background-color: #f7f7ff;
+}
+
+.sidebar-container {
+  border-radius: 4px;
+  background-color: #00000000;
+}
+
+.user-info {
+  display: flex;
+  align-items: center;
+  color: #666;
+}
+
+.user-name:hover,
+.notice-item:hover,
+.user-content:hover {
+  color: #4d45e5;
+}
+
+.notice-card,
+.rank-card,
+.recommend-category-card {
+  background-color: #ffffff;
+  border-radius: 4px;
+}
+
+.rank-card,
+.recommend-category-card {
+  margin-top: 15px;
+}
+
+.notice-title,
+.rank-title,
+.recommend-title {
+  font-weight: 700;
+  font-size: 16px;
+  padding: 0 15px;
+  border-bottom: 1px solid #f4f4f4;
+  height: 54px;
+  display: flex;
+  align-items: center;
+}
+
+.notice-content,
+.recommend-content {
+  padding: 15px;
+}
+
+.recommend-content {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.recommend-content a {
+  background-color: #f1f1ff;
+  color: #666;
+  border-radius: 4px;
+  font-size: 13px;
+  margin: 7px 0;
+  padding: 3px 15px;
+  text-align: center;
+  width: 45%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  cursor: pointer;
+}
+
+.recommend-content a:hover {
+  background-color: #6b64ee;
+  color: #fff;
+}
+
+.notice-item,
+.rank-item {
+  display: flex;
+  justify-content: space-between;
+  color: #666;
+  align-items: center;
+}
+
+.rank-item {
+  font-size: 14px;
+  padding: 10px 15px;
+}
+
+.user-content {
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+}
+
+.user-desc {
+  font-size: 12px;
+}
+
+@media screen and (max-width: 1200px) {
+  .nav-box,
+  .content-box {
+    padding: 0 10px;
+  }
+}
+
+@media screen and (max-width: 800px) {
+  .content-box {
+    padding: 0;
+  }
+
+  .sidebar-container {
+    display: none;
+  }
+
+  .article-items-container {
+    margin: 0;
+  }
+
+  .content-box {
+    margin-top: 10px;
+  }
+}
+</style>
