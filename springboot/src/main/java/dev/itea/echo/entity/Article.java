333b51072147e40a6dd3cdda06257fe8f69d3a34
isixe <isixe@outlook.com>
2024-02-14 11:06:40 +0800

    :zap: perf(entity&mapper): synchronize entity form article table

--- springboot/src/main/java/dev/itea/echo/entity/Article.java
+++ springboot/src/main/java/dev/itea/echo/entity/Article.java
@@ -22,7 +22,7 @@ import org.hibernate.validator.constraints.Length;
  * 文章表
  *
  * @author isixe
- * @since 2024-01-15
+ * @since 2024-02-13
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -47,12 +47,20 @@ public class Article extends Model<Article> {
     @TableField("user_id")
     private Integer userId;
 
+
     @Schema(description = "文章标题")
     @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     @NotBlank(message = "不能为空字符串", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     @Length(message = "长度不能大于42个字符", max = 16, groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     @TableField("title")
     private String title;
+
+    @Schema(description = "副标题")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @NotBlank(message = "不能为空字符串", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @Length(message = "长度不能大于200个字符", max = 200, groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @TableField("sub_title")
+    private String subTitle;
 
     @Schema(description = "文章内容")
     @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
@@ -85,15 +93,18 @@ public class Article extends Model<Article> {
     @TableField("collecion_count")
     private Integer collecionCount;
 
+    @Schema(description = "创建时间")
+    @TableField("created_time")
+    private LocalDateTime createdTime;
+
+    @Schema(description = "发布时间")
+    @TableField("publish_time")
+    private LocalDateTime publishTime;
+
     @Schema(description = "更新时间")
     @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE, updateStrategy = FieldStrategy.NOT_NULL)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime updateTime;
-
-    @Schema(description = "发布时间")
-    @TableField(value = "created_time", fill = FieldFill.INSERT, updateStrategy = FieldStrategy.NOT_NULL)
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private LocalDateTime createdTime;
 
     @Schema(description = "是否删除")
     @TableField("is_deleted")
