ab9a9cb84f6e8abc384bd6ad685336aef228d0a0
isixe <isixe@outlook.com>
2024-02-16 10:44:22 +0800

    ✨ feat(api): add the basic API of the article

--- vue/src/api/article.js
+++ vue/src/api/article.js
@@ -1,5 +1,45 @@
 import request from '@/utils/request'
 
+export function add(data) {
+  return request({
+    url: '/article',
+    method: 'post',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: '/article',
+    method: 'put',
+    data
+  })
+}
+
+export function remove(data) {
+  return request({
+    url: '/article',
+    method: 'delete',
+    data
+  })
+}
+
+export function get(params) {
+  return request({
+    url: '/article',
+    method: 'get',
+    params
+  })
+}
+
+export function getArticleList(params) {
+  return request({
+    url: '/article/queryAll',
+    method: 'get',
+    params
+  })
+}
+
 export async function getUserRank() {
   return request({
     url: '/article/userRank',
