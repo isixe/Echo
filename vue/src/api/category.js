3f226883b2594aa27cd09eb113ba6ab124372a75
isixe <isixe@outlook.com>
2024-02-16 16:13:13 +0800

    ✨ feat(api): add category base api

---
+++ vue/src/api/category.js
@@ -0,0 +1,49 @@
+import request from '@/utils/request'
+
+export function add(data) {
+  return request({
+    url: '/category',
+    method: 'post',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: '/category',
+    method: 'put',
+    data
+  })
+}
+
+export function remove(data) {
+  return request({
+    url: '/category',
+    method: 'delete',
+    data
+  })
+}
+
+export function get(params) {
+  return request({
+    url: '/category',
+    method: 'get',
+    params
+  })
+}
+
+export function getCategoryList(params) {
+  return request({
+    url: '/category/queryAll',
+    method: 'get',
+    params
+  })
+}
+
+export function getCategoryListByName(params) {
+  return request({
+    url: '/category/queryByName',
+    method: 'get',
+    params
+  })
+}
