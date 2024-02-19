21094306b95f470e7d39a8b439a94a5328240d3e
isixe <isixe@outlook.com>
2024-02-19 18:53:26 +0800

    ✨ feat(main.js): use CKEditor

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import CKEditor from '@ckeditor/ckeditor5-vue'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
@@ -15,6 +16,7 @@ const pina = createPinia().use(piniaPluginPersistedstate)
 
 app.use(pina)
 app.use(router)
+app.use(CKEditor)
 app.use(autoAnimatePlugin)
 app.component('font-awesome-icon', FontAwesomeIcon)
 
