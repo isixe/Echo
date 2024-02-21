9e8100f4b87a598fa0d5f4ed9d0f241c3df12ec4
isixe <isixe@outlook.com>
2024-02-21 09:00:25 +0800

    :bug: fix(config): fixed redis LocalDateTime Serializer

--- springboot/src/main/java/dev/itea/echo/config/RedisConfig.java
+++ springboot/src/main/java/dev/itea/echo/config/RedisConfig.java
@@ -4,7 +4,13 @@ package dev.itea.echo.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -17,11 +23,14 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 /**
  * RedisTemplate 配置类
@@ -33,9 +42,37 @@ import java.time.Duration;
 @EnableCaching
 public class RedisConfig {
 
-    private final ObjectMapper objectMapper = new ObjectMapper()
-            .setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
-            .activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+    private final GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer;
+
+    public RedisConfig() {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+                .activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        //设置timeModule手动序列化
+        JavaTimeModule timeModule = new JavaTimeModule();
+        // LocalDateTime和LocalDate序列化
+        timeModule.addSerializer(LocalDateTime.class,
+                new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                        .withZone(ZoneId.of("Asia/Shanghai"))))
+
+                .addDeserializer(LocalDateTime.class,
+                        new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                .withZone(ZoneId.of("Asia/Shanghai"))))
+
+                .addSerializer(LocalDate.class,
+                        new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                                .withZone(ZoneId.of("Asia/Shanghai"))))
+
+                .addDeserializer(LocalDate.class,
+                        new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                                .withZone(ZoneId.of("Asia/Shanghai"))));
+        // 设置自定义时间模块
+        objectMapper.registerModule(timeModule);
+
+        this.jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+    }
 
     /**
      * 初始化RedisConnectionFactory
@@ -53,9 +90,6 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
-
-        //初始化 jackson2Json redis Serializer，设置 ObjectMapper
-        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
         // String     - StringRedisSerializer 序列化
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
@@ -76,14 +110,18 @@ public class RedisConfig {
      */
     @Bean
     public CacheManager cacheManager() {
-        //初始化 jackson2Json redis Serializer，设置 ObjectMapper
-        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-
         //配置redisCacheConfiguration缓存设置
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofSeconds(5000))
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer)).disableCachingNullValues();
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(jackson2JsonRedisSerializer))
+
+                .disableCachingNullValues();
 
         //初始化RedisCacheManager实例
         return RedisCacheManager.builder(redisConnectionFactory())
