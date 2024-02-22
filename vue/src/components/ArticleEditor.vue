7c7dc2c29967f1ce4ae2132f64ee089b5b430f06
isixe <isixe@outlook.com>
2024-02-22 15:16:09 +0800

    🦄 refactor(view): use wangeditor5 replace ckeditor5

--- vue/src/components/ArticleEditor.vue
+++ vue/src/components/ArticleEditor.vue
@@ -1,44 +1,61 @@
 <template>
-  <ckeditor
-    :editor="editor"
-    v-model="editorData"
-    :config="editorConfig"
-    @ready="onReady"
-  ></ckeditor>
+  <div style="border: 1px solid #ccc">
+    <Toolbar
+      style="border-bottom: 1px solid #ccc"
+      :editor="editorRef"
+      :defaultConfig="toolbarConfig"
+    />
+    <Editor
+      style="height: 500px; overflow-y: hidden"
+      v-model="valueHtml"
+      :defaultConfig="editorConfig"
+      @onCreated="handleCreated"
+    />
+  </div>
 </template>
 
 <script setup>
+import '@wangeditor/editor/dist/css/style.css'
+
 import { uploadPic } from '@/api/file'
 import { message } from 'ant-design-vue'
-import Editor from 'ckeditor5-custom-build/build/ckeditor'
 
-const editor = Editor
-const editorData = defineModel('editorData')
+import { Boot } from '@wangeditor/editor'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+import markdownModule from '@wangeditor/plugin-md'
+Boot.registerModule(markdownModule)
+
+const editorRef = shallowRef()
+const valueHtml = defineModel()
+
+const toolbarConfig = {}
+
+toolbarConfig.excludeKeys = ['insertVideo', 'uploadVideo', 'editVideoSize']
+
 const editorConfig = {
-  removePlugins: ['MediaEmbedToolbar']
+  placeholder: '请输入内容...',
+  MENU_CONF: {}
 }
 
-class ImageUploadAdapter {
-  constructor(loader) {
-    this.loader = loader
-  }
-  async upload() {
+editorConfig.MENU_CONF['uploadImage'] = {
+  async customUpload(file, insertFn) {
     const formData = new FormData()
-    formData.append('file', await this.loader.file)
-    const data = await uploadPic(formData).then((res) => {
+    formData.append('file', file)
+    const url = await uploadPic(formData).then((res) => {
       message.success('上传成功')
       return res.data
     })
-
-    return {
-      default: data
-    }
+    insertFn(url)
   }
 }
 
-const onReady = (editor) => {
-  editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-    return new ImageUploadAdapter(loader)
-  }
+onBeforeUnmount(() => {
+  const editor = editorRef.value
+  if (editor == null) return
+  editor.destroy()
+})
+
+const handleCreated = (editor) => {
+  editorRef.value = editor
 }
 </script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import CKEditor from '@ckeditor/ckeditor5-vue'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
@@ -16,7 +15,6 @@ const pina = createPinia().use(piniaPluginPersistedstate)
 
 app.use(pina)
 app.use(router)
-app.use(CKEditor)
 app.use(autoAnimatePlugin)
 app.component('font-awesome-icon', FontAwesomeIcon)
 
