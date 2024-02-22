1a1dbccd5c8c6a4e977c132f4b7112daecc36515
isixe <isixe@outlook.com>
2024-02-22 20:41:29 +0800

    ✨ feat(view): add article edit view

---
+++ vue/src/views/page-home/article/articleEdit.vue
@@ -0,0 +1,233 @@
+<template>
+  <div class="container">
+    <div class="left">
+      <div class="left-item">
+        <label>头图：</label>
+        <pic-upload v-model="data.featuredPic" />
+      </div>
+      <div class="left-item">
+        <label>标签：</label>
+        <tag-input v-model="data.tag" />
+      </div>
+      <div class="left-item">
+        <label>类别：</label>
+        <category-select
+          v-model:categoryId="data.categoryId"
+          v-model:categoryName="data.category"
+        />
+      </div>
+      <div class="left-item">
+        <label>分组：</label>
+        <article-group-select
+          v-model:userId="data.userId"
+          v-model:articleGroupId="data.articleGroupId"
+          v-model:articleGroupName="data.articleGroupName"
+        />
+      </div>
+    </div>
+    <div class="right">
+      <a-input
+        class="title"
+        v-model:value="data.title"
+        placeholder="文章标题"
+        show-count
+        :maxlength="42"
+      />
+      <article-editor class="right-content" v-model="data.content" />
+      <div class="right-footer">
+        <div class="right-footer-left">
+          <template v-if="data.publishTime">
+            <a-date-picker v-model:value="data.publishTime" disabled />
+            <a-time-picker v-model:value="data.publishTime" style="margin-left: 10px" disabled />
+          </template>
+          <template v-else>
+            <a-date-picker v-model:value="data.publishTime" />
+            <a-time-picker v-model:value="data.publishTime" style="margin-left: 10px" />
+          </template>
+        </div>
+
+        <div class="right-footer-right">
+          <template v-if="data.content && data.status !== 1">
+            <a-button
+              type="primary"
+              style="background-color: #008000; margin-right: 10px"
+              @click="draftArticle()"
+              :loading="loading"
+              >保存为草稿</a-button
+            >
+          </template>
+          <template v-else>
+            <a-button
+              type="primary"
+              style="background-color: #ccc; margin-right: 10px; color: #ffffff"
+              disabled
+              >保存为草稿</a-button
+            >
+          </template>
+          <template v-if="data.publishTime && data.content">
+            <a-button
+              type="primary"
+              style="background-color: #4d45e5"
+              @click="postArticle()"
+              :loading="loading"
+              >发布</a-button
+            >
+          </template>
+
+          <template v-else>
+            <a-button type="primary" style="background-color: #ccc; color: #ffffff" disabled
+              >发布</a-button
+            >
+          </template>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import dayjs from 'dayjs'
+import { DateTime } from 'luxon'
+import { message } from 'ant-design-vue'
+import { useUserStore } from '@/stores/user'
+import { get, add, update } from '@/api/article'
+import { TagInput, CategorySelect, ArticleGroupSelect, PicUpload } from '@/components'
+import ArticleEditor from '@/components/ArticleEditor'
+
+const loading = ref()
+const route = useRoute()
+const router = useRouter()
+const data = reactive({
+  id: '',
+  category: '',
+  status: null,
+  articleGroupName: '',
+  title: '',
+  featuredPic: '',
+  summary: '',
+  content: '',
+  userId: '',
+  categoryId: '',
+  tag: '',
+  articleGroupId: '',
+  publishTime: ''
+})
+
+onMounted(() => {
+  if (!route.query.id) {
+    return
+  }
+  get({ id: route.query.id }).then((res) => {
+    Object.keys(data).forEach((key) => {
+      data[key] = res.data[key]
+    })
+
+    data.publishTime = dayjs(data.publishTime, 'YYYY-MM-DD HH:mm:ss')
+    console.log(res.data)
+  })
+})
+
+const draftArticle = () => {
+  data.summary = data.content.replace(/<[^>]+>|\n/g, '').substring(0, 120)
+  const formData = new FormData()
+  Object.keys(data).forEach((key) => {
+    formData.append(key, data[key])
+  })
+  formData.set('status', 0)
+  addArticle(formData)
+}
+
+const postArticle = () => {
+  data.summary = data.content.replace(/<[^>]+>|\n/g, '').substring(0, 120)
+  const formData = new FormData()
+  Object.keys(data).forEach((key) => {
+    formData.append(key, data[key])
+  })
+  formData.set(
+    'publishTime',
+    DateTime.fromJSDate(data.publishTime.$d).toFormat("yyyy-MM-dd'T'HH:mm:ss")
+  )
+
+  if (data.status) {
+    formData.set('status', 1)
+    updateArticle(formData)
+  } else {
+    formData.set('status', 1)
+    addArticle(formData)
+  }
+}
+
+const addArticle = (formData) => {
+  const store = useUserStore()
+  formData.set('userId', store.id)
+
+  add(formData)
+    .then(() => {
+      message.success('发布成功')
+      loading.value = false
+      router.push('/article')
+    })
+    .catch(() => {
+      loading.value = false
+    })
+}
+
+const updateArticle = (formData) => {
+  update(formData)
+    .then(() => {
+      message.success('更新成功')
+      router.push('/article')
+      loading.value = false
+    })
+    .catch(() => {
+      loading.value = false
+    })
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 15px;
+  background-color: #ffffff;
+  display: flex;
+}
+
+.left {
+  flex: 1;
+}
+
+.left-item {
+  margin-bottom: 15px;
+  display: flex;
+  flex-direction: row;
+}
+
+.left-item span {
+  margin-left: 5px;
+}
+
+.left-item label {
+  width: 55px;
+  margin-right: 10px;
+}
+
+.right {
+  width: 735px;
+}
+
+.right-content {
+  margin-bottom: 10px;
+}
+
+.right .title {
+  height: 40px;
+  font-size: 16px;
+  border-radius: 0;
+}
+
+.right-footer {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 25px;
+}
+</style>
