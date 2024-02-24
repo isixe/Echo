67cf70e3ea4e0351ef2eaf72f44627c4bb2cf832
isixe <isixe@outlook.com>
2024-02-24 17:52:11 +0800

    :zap: perf(entity): support field validation

--- springboot/src/main/java/dev/itea/echo/entity/CommentArticle.java
+++ springboot/src/main/java/dev/itea/echo/entity/CommentArticle.java
@@ -2,22 +2,28 @@ package dev.itea.echo.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.io.Serial;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.itea.echo.validation.AddValidationGroup;
+import dev.itea.echo.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 /**
-* 文章评论表
-*
-* @author isixe
-* @since 2024-01-15
-*/
+ * 文章评论表
+ *
+ * @author isixe
+ * @since 2024-01-15
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
@@ -29,14 +35,17 @@ public class CommentArticle extends Model<CommentArticle> {
 
     @Schema(description = "ID")
     @TableId(value = "id", type = IdType.AUTO)
+    @NotNull(message = "不能为空", groups = {UpdateValidationGroup.class})
     private Integer id;
 
     @Schema(description = "用户ID")
     @TableField("user_id")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     private Integer userId;
 
     @Schema(description = "文章ID")
     @TableField("article_id")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     private Integer articleId;
 
     @Schema(description = "顶层评论ID")
@@ -49,10 +58,21 @@ public class CommentArticle extends Model<CommentArticle> {
 
     @Schema(description = "评论内容")
     @TableField("content")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @NotBlank(message = "不能为空字符串", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     private String content;
 
+    @Schema(description = "支持数")
+    @TableField("like_count")
+    private Integer likeCount;
+
+    @Schema(description = "否定数")
+    @TableField("dislike_count")
+    private Integer dislikeCount;
+
     @Schema(description = "发送时间")
-    @TableField("created_time")
+    @TableField(value = "created_time", fill = FieldFill.INSERT_UPDATE, updateStrategy = FieldStrategy.NOT_NULL)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createdTime;
 
     @Schema(description = "是否删除")
