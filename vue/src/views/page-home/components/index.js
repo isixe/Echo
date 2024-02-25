5470ec636708eac839034d10cfab44b7390b3061
isixe <isixe@outlook.com>
2024-02-25 21:44:40 +0800

    🦄 refactor(components): rename component

--- vue/src/views/page-home/components/index.js
+++ vue/src/views/page-home/components/index.js
@@ -2,6 +2,6 @@ import Header from './Header.vue'
 import Footer from './Footer.vue'
 import Main from './Main.vue'
 import EntryItem from './EntryItem.vue'
-import CommentItem from './CommentItem.vue'
+import ArticleCommentItem from './ArticleCommentItem.vue'
 
-export { Header, Main, Footer, EntryItem, CommentItem }
+export { Header, Main, Footer, EntryItem, ArticleCommentItem }
