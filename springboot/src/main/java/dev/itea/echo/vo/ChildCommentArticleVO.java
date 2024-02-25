86b8a9629531e9efa7950dfc2e464daabf5488d5
isixe <isixe@outlook.com>
2024-02-25 21:30:42 +0800

    :sparkles: feat(vo): add article comment VO

--- springboot/src/main/java/dev/itea/echo/vo/ChildCommentArticleVO.java
+++ springboot/src/main/java/dev/itea/echo/vo/ChildCommentArticleVO.java
@@ -1,21 +1,25 @@
 package dev.itea.echo.vo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import dev.itea.echo.validation.AddValidationGroup;
+import dev.itea.echo.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
 
 /**
- * 文章评论值对象
+ * ca.root_comment_id,
  *
  * @author: isixe
- * @create: 2024-02-24 14:26
+ * @create: 2024-02-25 15:19
  **/
 @Data
 @Accessors(chain = true)
-public class CommentArticleVO {
+public class ChildCommentArticleVO {
 
     @Schema(description = "ID")
     private Integer id;
@@ -29,29 +33,30 @@ public class CommentArticleVO {
     @Schema(description = "作者头像")
     private String avatar;
 
-    @Schema(description = "文章ID")
-    private Integer articleId;
-
-    @Schema(description = "文章标题")
-    private String articleTitle;
-
-    @Schema(description = "顶层评论ID")
-    private Integer rootCommentId;
-
     @Schema(description = "关联父评论ID")
     private Integer parentCommentId;
+
+    @Schema(description = "父级用户ID")
+    private Integer parentUserId;
+
+    @Schema(description = "用户名")
+    private String parentUserName;
 
     @Schema(description = "评论内容")
     private String content;
 
+    @Schema(description = "引用评论内容")
+    private String parentContent;
+
     @Schema(description = "支持数")
+    @TableField("like_count")
     private Integer likeCount;
 
     @Schema(description = "否定数")
+    @TableField("dislike_count")
     private Integer dislikeCount;
 
     @Schema(description = "发送时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createdTime;
-
 }
--- springboot/src/main/java/dev/itea/echo/vo/RootCommentArticleVO.java
+++ springboot/src/main/java/dev/itea/echo/vo/RootCommentArticleVO.java
@@ -1,6 +1,9 @@
 package dev.itea.echo.vo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import dev.itea.echo.validation.AddValidationGroup;
+import dev.itea.echo.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -8,14 +11,14 @@ import lombok.experimental.Accessors;
 import java.time.LocalDateTime;
 
 /**
- * 文章评论值对象
+ * 文章评论顶级评论值对象
  *
  * @author: isixe
- * @create: 2024-02-24 14:26
+ * @create: 2024-02-25 15:16
  **/
 @Data
 @Accessors(chain = true)
-public class CommentArticleVO {
+public class RootCommentArticleVO {
 
     @Schema(description = "ID")
     private Integer id;
@@ -31,15 +34,6 @@ public class CommentArticleVO {
 
     @Schema(description = "文章ID")
     private Integer articleId;
-
-    @Schema(description = "文章标题")
-    private String articleTitle;
-
-    @Schema(description = "顶层评论ID")
-    private Integer rootCommentId;
-
-    @Schema(description = "关联父评论ID")
-    private Integer parentCommentId;
 
     @Schema(description = "评论内容")
     private String content;
