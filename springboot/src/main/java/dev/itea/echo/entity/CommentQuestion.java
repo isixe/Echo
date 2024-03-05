13736faf52f0a212ad26030c7a502e958417ad87
isixe <isixe@outlook.com>
2024-03-05 11:22:26 +0800

    :sparkles: feat(entity): support for data verification

--- springboot/src/main/java/dev/itea/echo/entity/CommentQuestion.java
+++ springboot/src/main/java/dev/itea/echo/entity/CommentQuestion.java
@@ -7,8 +7,13 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.io.Serial;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.itea.echo.validation.AddValidationGroup;
+import dev.itea.echo.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -30,14 +35,17 @@ public class CommentQuestion extends Model<CommentQuestion> {
 
     @Schema(description = "ID")
     @TableId(value = "id", type = IdType.AUTO)
+    @NotNull(message = "不能为空", groups = {UpdateValidationGroup.class})
     private Integer id;
 
     @Schema(description = "用户ID")
     @TableField("user_id")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     private Integer userId;
 
     @Schema(description = "问答ID")
     @TableField("question_id")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     private Integer questionId;
 
     @Schema(description = "顶层评论ID")
@@ -50,6 +58,8 @@ public class CommentQuestion extends Model<CommentQuestion> {
 
     @Schema(description = "评论内容")
     @TableField("content")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @NotBlank(message = "不能为空字符串", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     private String content;
 
     @Schema(description = "支持数")
@@ -61,7 +71,8 @@ public class CommentQuestion extends Model<CommentQuestion> {
     private Integer dislikeCount;
 
     @Schema(description = "发送时间")
-    @TableField("created_time")
+    @TableField(value = "created_time", fill = FieldFill.INSERT_UPDATE, updateStrategy = FieldStrategy.NOT_NULL)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createdTime;
 
     @Schema(description = "是否删除")
@@ -75,5 +86,4 @@ public class CommentQuestion extends Model<CommentQuestion> {
     public Serializable pkVal() {
         return this.id;
     }
-
 }
