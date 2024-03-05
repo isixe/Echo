3391dda31d2e45d20240e4792385f31e0a699dc5
isixe <isixe@outlook.com>
2024-03-05 13:56:21 +0800

    ✨ feat(api): add question edit router path

--- vue/src/router/module/user.js
+++ vue/src/router/module/user.js
@@ -3,6 +3,11 @@ const userRoutes = [
     path: '/article/edit',
     name: 'articleEdit',
     component: () => import('@/views/page-home/article/articleEdit')
+  },
+  {
+    path: '/question/edit',
+    name: 'questionEdit',
+    component: () => import('@/views/page-home/question/questionEdit')
   }
 ]
 
