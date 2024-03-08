d611dd1cb22cbf35944e80a64f2740ae5b7dce58
isixe <isixe@outlook.com>
2024-03-08 12:10:55 +0800

    ✨ feat(view): add search view

---
+++ vue/src/views/page-home/search/index.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="nav-bar">
+    <div class="inner-bar">
+      <a-menu
+        class="nav-menu"
+        v-model:selectedKeys="selectedKey"
+        mode="horizontal"
+        :items="items"
+      />
+      <div>
+        <a class="operation-filter" @click="() => (showFilter = !showFilter)"
+          ><FilterOutlined /> 筛选</a
+        >
+      </div>
+    </div>
+  </div>
+  <div class="nav-bar" v-show="showFilter">
+    <div class="filter-option">
+      <RouterLink
+        :to="{ path: 'search', query: { type: search.type, q: search.q } }"
+        :class="!search.type ? 'active' : ''"
+      >
+        综合排序
+      </RouterLink>
+      <RouterLink
+        :to="{ path: 'search', query: { type: search.type, sort: 'likeCount', q: search.q } }"
+        :class="search.type == 'likeCount' ? 'active' : ''"
+        >最多点赞</RouterLink
+      >
+      <RouterLink
+        :to="{ path: 'search', query: { type: search.type, sort: 'updateTime', q: search.q } }"
+        :class="search.type == 'updateTime' ? 'active' : ''"
+      >
+        最新发布
+      </RouterLink>
+    </div>
+  </div>
+
+  <div class="search-result">
+    <div class="search-content">
+      <template v-if="selectedKey == 'article'">
+        <template v-if="articleData">
+          <div v-for="item in articleData" :key="item.id">
+            <RouterLink :to="'/article/' + item.id">
+              <article-entry-item :item="item"></article-entry-item
+            ></RouterLink>
+          </div>
+        </template>
+        <template v-else> <a-empty style="padding-bottom: 30px" /></template>
+      </template>
+      <template v-else-if="selectedKey == 'question'">
+        <template v-if="questionData">
+          <div v-for="item in questionData" :key="item.id">
+            <RouterLink :to="'/question/' + item.id" style="display: flex; width: 100%">
+              <question-entry-item :item="item"></question-entry-item>
+            </RouterLink>
+          </div>
+        </template>
+        <template v-else> <a-empty style="padding-bottom: 30px" /></template>
+      </template>
+      <template v-else> <a-empty style="padding-bottom: 30px" /> </template>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ArticleEntryItem, QuestionEntryItem } from '@/views/page-home/components'
+import { getActiveArticleListByKeyword } from '@/api/article'
+import { getActiveQuestionListByKeyword } from '@/api/question'
+
+const route = useRoute()
+const router = useRouter()
+const articleData = ref()
+const questionData = ref()
+const selectedKey = ref(['article'])
+const showFilter = ref(false)
+
+const search = reactive({
+  type: route.query.type,
+  sort: route.query.sort,
+  q: route.query.q
+})
+
+watch(search, () => {
+  console.log(search)
+})
+
+const params = reactive({
+  pageNum: 1,
+  pageSize: 15,
+  keyword: route.query.q
+})
+
+onMounted(() => {
+  getArticleDataSource()
+
+  if (route.query.type) {
+    selectedKey.value = [route.query.type]
+  }
+})
+
+const getArticleDataSource = () => {
+  getActiveArticleListByKeyword(params).then((res) => {
+    articleData.value = res.data.records
+  })
+}
+
+const getQuestionDataSource = () => {
+  getActiveQuestionListByKeyword(params).then((res) => {
+    questionData.value = res.data.records
+  })
+}
+
+watch(selectedKey, (key) => {
+  switch (key[0]) {
+    case 'article':
+      router.push({ path: '/search', query: { type: 'article', q: route.query.q } })
+      getArticleDataSource(key[0])
+      break
+    case 'question':
+      router.push({ path: '/search', query: { type: 'question', q: route.query.q } })
+      getQuestionDataSource(key[0])
+      break
+  }
+})
+
+const items = ref([
+  {
+    key: 'article',
+    label: '文章'
+  },
+  {
+    key: 'question',
+    label: '问答'
+  }
+])
+</script>
+
+<style scoped>
+.nav-bar {
+  display: flex;
+  justify-content: center;
+}
+
+.inner-bar {
+  width: 1000px;
+  border-radius: 4px;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+}
+
+.search-result {
+  display: flex;
+  justify-content: center;
+  margin: 15px 0;
+}
+
+.search-content {
+  width: 1000px;
+  background-color: #ffffff;
+}
+
+.operation-filter {
+  color: #666;
+}
+
+.operation-filter:hover {
+  color: #4d45e5;
+}
+
+.filter-option {
+  background-color: #ffffff;
+  width: 1000px;
+  padding: 15px;
+}
+
+.filter-option a {
+  border-radius: 4px;
+  padding: 8px 12px;
+  margin-right: 10px;
+  font-size: 14px;
+  color: #666;
+}
+
+.filter-option .active {
+  background-color: rgba(23, 114, 246, 0.1);
+}
+
+.filter-option a:hover {
+  background-color: rgba(23, 114, 246, 0.1);
+  color: #4d45e5;
+  cursor: pointer;
+}
+</style>
