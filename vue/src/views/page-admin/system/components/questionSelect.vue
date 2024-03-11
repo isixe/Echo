1bff3f4979cc5481b667cb50c6bfcbe7523d3149
isixe <isixe@outlook.com>
2024-03-11 18:09:59 +0800

    🎈 perf(components): infinite loading

--- vue/src/views/page-admin/system/components/questionSelect.vue
+++ vue/src/views/page-admin/system/components/questionSelect.vue
@@ -5,8 +5,10 @@
     placeholder="选择关联问答"
     style="width: 300px"
     :options="questionOptions"
-    :filter-option="filterOption"
-    @search="authorSearch"
+    @focus="onQuestionFocus"
+    :filter-option="() => true"
+    @popupScroll="onPopupScroll"
+    @search="onQuestionSearch"
   ></a-select>
 </template>
 
@@ -14,18 +16,55 @@
 import { getQuestionListByTitle } from '@/api/question'
 
 const questionId = defineModel()
-
 const questionOptions = ref([])
 
-const filterOption = (input, option) => option.label.indexOf(input) >= 0
+const pages = ref(0)
+const params = reactive({
+  pageNum: 1,
+  pageSize: 15,
+  keyword: ''
+})
 
-const authorSearch = (value) => {
-  if (value === null || value === '') {
+const onQuestionSearch = (value) => {
+  params.keyword = value
+  params.pageNum = 1
+  getQuestionListByTitle(params).then((res) => {
+    const data = res.data.records
+    pages.value = res.data.pages
+    questionOptions.value = data.map((question) => ({ value: question.id, label: question.title }))
+  })
+}
+
+const onPopupScroll = (e) => {
+  const { scrollTop, offsetHeight, scrollHeight } = e.target
+  if (scrollTop + offsetHeight !== scrollHeight || params.pageNum >= pages.value) {
     return
   }
-  getQuestionListByTitle({ title: value }).then((res) => {
-    const data = res.data
-    questionOptions.value = data.map((question) => ({ value: question.id, label: question.title }))
+
+  params.pageNum += 1
+  getQuestionListByTitle(params).then((res) => {
+    const data = res.data.records
+    pages.value = res.data.pages
+    questionOptions.value.push(
+      ...data.map((question) => ({
+        value: question.id,
+        label: question.title
+      }))
+    )
+  })
+}
+
+const onQuestionFocus = () => {
+  params.pageNum = 1
+  getQuestionListByTitle(params).then((res) => {
+    const data = res.data.records
+    pages.value = res.data.pages
+    questionOptions.value = data.map((question) => ({
+      value: question.id,
+      label: question.title
+    }))
+
+    questionId.value = questionId.value ? parseInt(questionId.value) : null
   })
 }
 </script>
