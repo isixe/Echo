f476fed51b0dbcbe65a8adab162dc6dd7a0c5ee6
isixe <isixe@outlook.com>
2024-03-12 17:04:09 +0800

    perf(entity): add data validation

--- springboot/src/main/java/dev/itea/echo/entity/ArticleThumb.java
+++ springboot/src/main/java/dev/itea/echo/entity/ArticleThumb.java
@@ -8,7 +8,10 @@ import java.time.LocalDateTime;
 import java.io.Serial;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.itea.echo.validation.AddValidationGroup;
+import dev.itea.echo.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -21,18 +24,24 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
 @TableName("article_thumb")
-@Schema(description = "")
+@Schema(description = "文章点赞表")
 public class ArticleThumb extends Model<ArticleThumb> {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Schema(description = "ID")
+    @NotNull(message = "不能为空", groups = UpdateValidationGroup.class)
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
-    @TableField("userId")
+    @Schema(description = "用户ID")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @TableField("user_id")
     private Integer userId;
 
-    @TableField("articleId")
+    @Schema(description = "文章ID")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @TableField("article_id")
     private Integer articleId;
 
     @Schema(description = "是否删除")
--- springboot/src/main/java/dev/itea/echo/entity/QuestionThumb.java
+++ springboot/src/main/java/dev/itea/echo/entity/QuestionThumb.java
@@ -8,7 +8,10 @@ import java.time.LocalDateTime;
 import java.io.Serial;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.itea.echo.validation.AddValidationGroup;
+import dev.itea.echo.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -21,18 +24,23 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
 @TableName("question_thumb")
-@Schema(description = "")
+@Schema(description = "问答点赞表")
 public class QuestionThumb extends Model<QuestionThumb> {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Schema(description = "ID")
+    @NotNull(message = "不能为空", groups = UpdateValidationGroup.class)
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
-    @TableField("userId")
+    @Schema(description = "用户ID")
+    @NotNull(message = "不能为空", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @TableField("user_id")
     private Integer userId;
 
-    @TableField("questionId")
+    @Schema(description = "问答ID")
+    @TableField("question_id")
     private Integer questionId;
 
     @Schema(description = "是否删除")
