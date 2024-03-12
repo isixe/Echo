c24cbf9934119a8451abd173bcceb118ff1d4cac
isixe <isixe@outlook.com>
2024-03-12 09:29:09 +0800

    🎈 perf(components): infinite loading

--- vue/src/views/page-home/tag/index.vue
+++ vue/src/views/page-home/tag/index.vue
@@ -3,14 +3,14 @@
   <div class="container">
     <a-menu class="nav-menu" v-model:selectedKeys="selectedKey" mode="horizontal" :items="items" />
     <template v-if="selectedKey == 'article'">
-      <div v-for="item in articleData" :key="item.id">
+      <div v-for="item in articleFullList" :key="item.id">
         <RouterLink :to="'/article/' + item.id">
           <article-entry-item :item="item"></article-entry-item
         ></RouterLink>
       </div>
     </template>
     <template v-else-if="selectedKey == 'question'">
-      <div v-for="item in questionData" :key="item.id">
+      <div v-for="item in questionFullList" :key="item.id">
         <RouterLink :to="'/question/' + item.id" style="display: flex; width: 100%">
           <question-entry-item :item="item"></question-entry-item>
         </RouterLink>
@@ -25,10 +25,16 @@ import { getArticleListByTagName } from '@/api/article'
 import { getQuestionListByTagName } from '@/api/question'
 
 const route = useRoute()
-const articleData = ref()
-const questionData = ref()
+const router = useRouter()
+
+const articleFullList = ref([])
+const questionFullList = ref([])
 const tagName = ref(route.query.tagName)
-const selectedKey = ref(['article'])
+const selectedKey = ref([''])
+
+const initLoading = ref(true)
+const loading = ref(false)
+const pages = ref(0)
 const params = reactive({
   pageNum: 1,
   pageSize: 15,
@@ -36,31 +42,72 @@ const params = reactive({
 })
 
 onMounted(() => {
-  getArticleDataSource()
+  window.addEventListener('scroll', scrollBottom, true)
+
+  selectedKey.value = route.query.tab ? [route.query.tab] : ['article']
 })
+
+watch(selectedKey, () => {
+  articleFullList.value = []
+  questionFullList.value = []
+  params.pageNum = 1
+  getDataSource(selectedKey.value)
+})
+
+const scrollBottom = () => {
+  if (initLoading.value || loading.value || params.pageNum >= pages.value) {
+    return
+  }
+
+  const windowHeight = document.body.scrollHeight
+  const scrollHeight = window.innerHeight + window.scrollY
+  if (windowHeight - scrollHeight >= 1) {
+    return
+  }
+
+  onLoadMore()
+}
+
+const onLoadMore = () => {
+  loading.value = true
+  params.pageNum += 1
+  getDataSource(selectedKey.value)
+  nextTick(() => {
+    window.dispatchEvent(new Event('resize'))
+  })
+}
+
+const getDataSource = (tab) => {
+  switch (tab[0]) {
+    case 'article':
+      router.push({ path: '/tag/', query: { tagName: route.query.tagName, tab: 'article' } })
+      getArticleDataSource()
+      break
+    case 'question':
+      router.push({ path: '/tag/', query: { tagName: route.query.tagName, tab: 'question' } })
+      getQuestionDataSource()
+      break
+  }
+}
 
 const getArticleDataSource = () => {
   getArticleListByTagName(params).then((res) => {
-    articleData.value = res.data.records
+    articleFullList.value = articleFullList.value.concat(res.data.records)
+    pages.value = res.data.pages
+    initLoading.value = false
+    loading.value = false
   })
 }
 
 const getQuestionDataSource = () => {
   getQuestionListByTagName(params).then((res) => {
-    questionData.value = res.data.records
+    console.log(questionFullList.value)
+    questionFullList.value = questionFullList.value.concat(res.data.records)
+    pages.value = res.data.pages
+    initLoading.value = false
+    loading.value = false
   })
 }
-
-watch(selectedKey, (key) => {
-  switch (key[0]) {
-    case 'article':
-      getArticleDataSource(key[0])
-      break
-    case 'question':
-      getQuestionDataSource(key[0])
-      break
-  }
-})
 
 const items = ref([
   {
