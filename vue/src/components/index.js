f84232aebd061b3484977d3225b5dedc9fb61edd
isixe <isixe@outlook.com>
2024-03-12 12:44:45 +0800

    🎈 perf(components): rename import path

--- vue/src/components/index.js
+++ vue/src/components/index.js
@@ -1,7 +1,7 @@
-import TagInput from '../components/tagInput'
+import TagInput from '../components/TagInput'
 import ArticleEditor from '../components/ArticleEditor'
 import PicUpload from '../components/PicUpload'
-import CategorySelect from '../components/categorySelect'
-import ArticleGroupSelect from '../components/articleGroupSelect'
+import CategorySelect from '../components/CategorySelect'
+import ArticleGroupSelect from '../components/ArticleGroupSelect'
 
 export { TagInput, ArticleEditor, PicUpload, CategorySelect, ArticleGroupSelect }
