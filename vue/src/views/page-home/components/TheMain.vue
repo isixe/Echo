c1216f51f6a695a35d4fdea9cff72422eb95bb35
isixe <isixe@outlook.com>
2024-03-17 19:52:10 +0800

    🌈 style(components): update UI style

--- vue/src/views/page-home/components/TheMain.vue
+++ vue/src/views/page-home/components/TheMain.vue
@@ -1,9 +1,7 @@
 <template>
-  <a-layout>
-    <a-layout-content>
-      <router-view></router-view>
-    </a-layout-content>
-  </a-layout>
+  <a-layout-content>
+    <router-view></router-view>
+  </a-layout-content>
 </template>
 
 <script setup></script>
