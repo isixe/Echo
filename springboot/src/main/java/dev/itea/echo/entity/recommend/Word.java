fca3880776a0945abc486a32743970a5bf186345
isixe <isixe@outlook.com>
2024-03-20 11:22:10 +0800

    :sparkles: feat(entity): add segmentation object

---
+++ springboot/src/main/java/dev/itea/echo/entity/recommend/Word.java
@@ -0,0 +1,52 @@
+package dev.itea.echo.entity.recommend;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+/**
+ * @author: isixe
+ * @create: 2024-03-20 08:42
+ * @description: 分词对象
+ **/
+@Data
+@NoArgsConstructor
+public class Word implements Comparable<Word> {
+    private String name;
+    private String pos;
+    private Float weight;
+
+    public Word(String name, String pos) {
+        this.name = name;
+        this.pos = pos;
+    }
+
+    @Override
+    public int compareTo(Word word) {
+        if (word == null || word.name == null) {
+            return 1;
+        }
+
+        if (this.name == null) {
+            return -1;
+        }
+
+        return this.name.compareTo(word.name);
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (name != null) {
+            stringBuilder.append(name);
+        }
+        if (pos != null) {
+            stringBuilder.append("/").append(pos);
+        }
+
+        return stringBuilder.toString();
+    }
+
+}
