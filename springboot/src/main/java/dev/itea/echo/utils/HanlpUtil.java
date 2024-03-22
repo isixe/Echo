70c7af3918173341d2fc8cdcb6241623101faa3e
isixe <isixe@outlook.com>
2024-03-22 15:23:01 +0800

    :sparkles: feat(utils): add HanLp segment util

--- springboot/src/main/java/dev/itea/echo/utils/HanlpUtil.java
+++ springboot/src/main/java/dev/itea/echo/utils/HanlpUtil.java
@@ -1,5 +1,6 @@
 package dev.itea.echo.utils;
 
+import ch.qos.logback.core.util.COWArrayList;
 import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.seg.common.Term;
 import dev.itea.echo.entity.recommend.Word;
@@ -10,8 +11,11 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,23 +25,33 @@ import java.util.stream.Stream;
  * @author: isixe
  * @create: 2024-03-19 20:34
  **/
-@Slf4j
 public class HanlpUtil {
 
     private final static String STOP_WORD_DICT_PATH = "/data/dictionary/stopwords.txt";
+    private static Set<String> stopwords;
+
+    public static Set<String> getStopwords() {
+        return stopwords;
+    }
+
+    static {
+        try {
+            // Load stopwords into a set for faster lookup
+            String classpath = ResourceUtils.getURL("classpath:").getPath().substring(1);
+            Path targetPath = Paths.get(classpath, STOP_WORD_DICT_PATH);
+            Stream<String> lines = Files.lines(targetPath);
+            stopwords = lines.collect(Collectors.toSet());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * get HanLP segment
      *
      * @param sentence text
      */
-    public static List<Word> segment(String sentence) throws IOException {
-        // Load stopwords into a set for faster lookup
-        String classpath = ResourceUtils.getURL("classpath:").getPath().substring(1);
-        Path targetPath = Paths.get(classpath, STOP_WORD_DICT_PATH);
-        Stream<String> lines = Files.lines(targetPath);
-        Set<String> stopwords = lines.collect(Collectors.toSet());
-
+    public static List<Word> segment(String sentence) {
         // Segment the sentence
         List<Term> termList = HanLP.segment(sentence);
 
@@ -45,71 +59,10 @@ public class HanlpUtil {
         termList.removeIf(term -> stopwords.contains(term.word));
 
         // Convert to Word objects
-        return termList.stream()
-                .filter(term -> !term.word.equals(" "))
-                .map(term -> new Word(term.word, term.nature.toString()))
-                .collect(Collectors.toList());
+        return new ArrayList<>(termList.stream()
+                .filter(term -> !term.word.equals(" ") && term.word.length() > 1)
+                .map(term -> new Word(term.word, term.nature.toString(), 0.0))
+                .collect(Collectors.toMap(Word::getName, Function.identity(), (existing, replacement) -> existing))
+                .values());
     }
-
-    /**
-     * compute TF-IDF（TF * IDF）
-     *
-     * @param set      sentence set
-     * @param sentence text
-     * @param word     segment
-     */
-    public static double getTF_IDF(Set<String> set, String sentence, String word) throws IOException {
-        double tf = computeTF(sentence, word);
-        double idf = computeIDF(set, word);
-        log.info("TF-IDF（TF * IDF） with segment: {}", String.format("%.3f", idf));
-        return tf * idf;
-    }
-
-    /**
-     * Compute TF (Term Frequency)
-     *
-     * @param sentence text
-     * @param word     segment
-     */
-    public static double computeTF(String sentence, String word) throws IOException {
-        // Load stopwords into a set for faster lookup
-        String classpath = ResourceUtils.getURL("classpath:").getPath().substring(1);
-        Path targetPath = Paths.get(classpath, STOP_WORD_DICT_PATH);
-        Stream<String> lines = Files.lines(targetPath);
-        Set<String> stopwords = lines.collect(Collectors.toSet());
-
-        // Segment the sentence
-        List<Term> termList = HanLP.segment(sentence);
-
-        // Remove stopwords
-        termList.removeIf(term -> stopwords.contains(term.word));
-
-        // Calculate term frequency
-        long count = termList.stream().filter(term -> word.equals(term.word)).count();
-        double tf = (double) count / termList.size();
-        log.info("TF (Term Frequency) in the segment sentence: {}", String.format("%.3f", tf));
-        return tf;
-    }
-
-    /**
-     * Compute IDF (Inverse Document Frequency)
-     *
-     * @param set  sentence set
-     * @param word segment
-     */
-    public static double computeIDF(Set<String> set, String word) throws IOException {
-        int count = 0;
-        for (String s : set) {
-            List<Word> words = segment(s);
-            for (Word w : words) {
-                if (w.getName().equals(word)) {
-                    count++;
-                }
-            }
-        }
-        double idf = Math.log((double) count / set.size());
-        log.info("IDF (Inverse Document Frequency) in all document: {}", String.format("%.3f", idf));
-        return idf;
-    }
-
 }
