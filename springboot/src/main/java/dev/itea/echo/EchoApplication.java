488a778991329dfdf1804659511ce4512c37562f
isixe <isixe@outlook.com>
2024-03-25 11:42:54 +0800

    :zap: perf(application): enable scheduled task

--- springboot/src/main/java/dev/itea/echo/EchoApplication.java
+++ springboot/src/main/java/dev/itea/echo/EchoApplication.java
@@ -3,8 +3,10 @@ package dev.itea.echo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @MapperScan(basePackages = "dev.itea.echo.mapper")
 public class EchoApplication {
 
